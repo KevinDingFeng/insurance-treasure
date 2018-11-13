383557f6203a0645547bf5e37a3316d458df5662
zdd382867915 <382867915@qq.com>
2018-11-13 09:45:09 +0800

    update

---
+++ insurance-treasure-entity/src/main/java/com/shenghesun/treasure/system/model/FundShow.java
@@ -0,0 +1,29 @@
+package com.shenghesun.treasure.system.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.shenghesun.treasure.entity.base.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Table
+@Data  
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class FundShow extends BaseEntity implements Serializable{
+
+	private static final long serialVersionUID = -7917411850934668617L;
+
+	//资金数
+	//@Column(columnDefinition="DECIMAL(16,2)")
+	private Integer orderAmount;
+	
+	//资金状态，入账还是出账，0代表出账 1代表入账
+	//@Column
+	private String plusOrMinus;
+	
+}
