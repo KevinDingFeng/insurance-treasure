99838951097b91a0bbae82b16c55acd00ce73773
yangzp <yangzp@shenghesun.com>
2018-11-20 16:40:39 +0800

    update

---
+++ insurance-treasure-interface/src/main/java/com/shenghesun/treasure/core/constant/Presentation.java
@@ -0,0 +1,50 @@
+package com.shenghesun.treasure.core.constant;
+
+public class Presentation {
+
+	/**
+	 * 主键
+	 */
+	public static final String KEY_ID = "id";
+
+	/**
+	 * 实体
+	 */
+	public static final String KEY_ENTITY = "entity";
+
+
+	/**
+	 * 翻页信息
+	 */
+	public static final String KEY_PAGE = "page";
+	/**
+	 * 默认翻页每页显示条数
+	 */
+	public static final int DEFAULT_PAGE_SIZE = 20;
+
+	/**
+	 * 默认翻页排序字段
+	 */
+	public static final String DEFAULT_ORDER_FIELD = "lastModified";
+
+	/**
+	 * 查询条件
+	 */
+	public static final String KEY_KEYWORD = "keyword";
+	
+	/**
+	 * 查询条件
+	 */
+	public static final String KEY_CONDITION = "condition";
+
+	/**
+	 * 成功
+	 */
+	public static final String KEY_SUCCESS = "success";
+
+	/**
+	 * 失败
+	 */
+	public static final String KEY_FAILURE = "failure";
+
+}
